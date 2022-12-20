910b1d483c4f051be53471de3a8be30938d07733
Deepasijoy <110151209+Deepasijoy@users.noreply.github.com>
2022-12-20 18:01:14 +0530

    error on execution.
    error

---
+++ webApp/src/main/java/com/greatlearning/web/HelloController.java
@@ -0,0 +1,16 @@
+package com.greatlearning.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String giveWelcomeMessage(ModelMap model) {
+		model.addAttribute("message","Spring MVC thymeleaf Hello World");
+		return "welcome";
+	}
+
+}
